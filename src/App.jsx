5022992e6f91f4586402ff36b11e815584c4e338
luthfizone <luthfizonedev@gmail.com>
2024-12-30 17:53:19 +0700

    feat: add note to note list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (note) => {
+    /**
+     * add new note to array notes
+     * with spread parameter
+     *
+     * setNotes([...notes, note])
+     *
+     * or can use with manual style
+     */
+    const newNote = [...notes];
+    newNote.push(note);
+    setNotes(newNote);
+  };
   return (
     <div className="app">
       <Header />
-      <InputNotes />
-      <NotesList />
+      <InputNotes addNote={addNote} />
+      <NotesList notes={notes} />
     </div>
   );
 }
@@ -12,27 +29,42 @@ function Header() {
   return <h1 className="header">Notes</h1>;
 }
 
-function InputNotes() {
+function InputNotes({ addNote }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim()) {
+      addNote(inputValue);
+      setInputValue("");
+    }
+  };
+
   return (
-    <form className="input-form">
+    <form className="input-form" onSubmit={handleSubmit}>
       <input
         type="text"
         name="notes"
         id="notes"
         className="input-field"
         placeholder="Add a new note..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
       />
       <button className="add-button">Add</button>
     </form>
   );
 }
 
-function NotesList() {
+function NotesList({ notes }) {
   return (
     <ul className="notes-list">
-      <li className="note-item">
-        Makan <button className="done-button">Done</button>
-      </li>
+      {notes.map((note, index) => (
+        <li key={index} className="note-item">
+          {note} <button className="done-button">Done</button>
+        </li>
+      ))}
     </ul>
   );
 }
